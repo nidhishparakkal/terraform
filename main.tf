1a6b2753137e3a3befe0e9e3f40c9bfc432aa468
root <root@ip-172-31-16-72.ap-southeast-1.compute.internal>
2018-11-22 10:41:17 +0000

    First day commit

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
-# Download the ghost:latest docker_image "image_id" to the system using a Terraform resource
-resource "docker_image" "image_id" {
-  name = "ghost:latest"
-}
-
-resource "docker_image" "image_id2" {
+# Download the docker images
+resource "docker_image" "ubuntu" {
   name = "nginx:latest"
 }
 
+# Create a container
+resource "docker_container" "ubuntu" {
+  image = "${docker_image.ubuntu.latest}"
+  name  = "ubuntu_container"
+  must_run = "false"  
+}
